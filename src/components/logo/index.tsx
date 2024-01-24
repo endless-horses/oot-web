9d110d107ba007e8c00324fd523eddf613f904ce
Jinwoo Lee <binaryWooo@gmail.com>
2024-01-25 01:12:00 +0900

    feat: logo component 구현 #4

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,15 @@
+import { Wrapper } from "./index.style";
+import defaultLogo from "@assets/icon/logo-default.svg";
+import inversionLogo from "@assets/icon/logo-inversion.svg";
+
+
+interface LogoProps {
+  inversion: boolean;
+  type?: 'small' | 'large';
+}
+
+function Logo(props: LogoProps) {
+  return <Wrapper image={props.inversion ? inversionLogo : defaultLogo} type={props.type} />;
+}
+
+export default Logo;
